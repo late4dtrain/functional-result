ca9a09a3b5114e5db2616510de9daab0c5981004
Gab Soulavy <1517510+gsoulavy@users.noreply.github.com>
2022-01-21 10:08:49 +0000

    reorganise solution (#3)

---
+++ Late4dTrain.Result.Tests/OnFailTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Late4dTrain;
+
+using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+public class OnFailTests
+{
+    [Fact]
+    public void OnFailWithIntResult()
+    {
+        var successCalled = false;
+        var failCalled = false;
+        var capturedError = string.Empty;
+
+        var result = Result<string>.Fail<int>("It went wrong...");
+        var func = () => result.Value;
+
+        result
+            .OnSuccess((i) => successCalled = true)
+            .OnFailure((e) =>
+                {
+                    failCalled = true;
+                    capturedError = e;
+                }
+            );
+
+        successCalled.Should().BeFalse();
+        failCalled.Should().BeTrue();
+        result.Error.Should().Be("It went wrong...");
+        capturedError.Should().Be("It went wrong...");
+        func.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void OnFailWithoutResult()
+    {
+        var successCalled = false;
+        var failCalled = false;
+        var capturedError = string.Empty;
+
+        var result = Result<string>.Fail("It went wrong...");
+
+        result
+            .OnSuccess(() => successCalled = true)
+            .OnFailure((e) =>
+                {
+                    failCalled = true;
+                    capturedError = e;
+                }
+            );
+
+        successCalled.Should().BeFalse();
+        failCalled.Should().BeTrue();
+        result.Error.Should().Be("It went wrong...");
+        capturedError.Should().Be("It went wrong...");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Late4dTrain.Functional.Result
 
-[![.NET](https://github.com/late4dtrain/functional-result/actions/workflows/ci.yml/badge.svg)](https://github.com/late4dtrain/functional-result/actions/workflows/ci.yml)
-[![Release to Nuget](https://github.com/late4dtrain/functional-result/actions/workflows/release.yml/badge.svg)](https://github.com/late4dtrain/functional-result/actions/workflows/release.yml)
-[![NuGet version (GSoulavy.Csv.Net)](https://img.shields.io/nuget/v/Late4dTrain.Functional.Result.svg?style=flat-square)](https://www.nuget.org/packages/Late4dTrain.Functional.Result/)
+[![.NET](https://github.com/late4dtrain/result/actions/workflows/ci.yml/badge.svg)](https://github.com/late4dtrain/result/actions/workflows/ci.yml)
+[![Release to Nuget](https://github.com/late4dtrain/result/actions/workflows/release.yml/badge.svg)](https://github.com/late4dtrain/result/actions/workflows/release.yml)
+[![NuGet version (GSoulavy.Csv.Net)](https://img.shields.io/nuget/v/Late4dTrain.Result.svg?style=flat-square)](https://www.nuget.org/packages/Late4dTrain.Result/)
 
-Functional Result Object
+Result Object
