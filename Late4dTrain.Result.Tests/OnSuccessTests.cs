abbe5f697eb2115d91442233fd1a760e28928154
Gab Soulavy <1517510+gsoulavy@users.noreply.github.com>
2022-01-21 10:06:02 +0000

    reorganise solution

---
+++ Late4dTrain.Result.Tests/OnSuccessTests.cs
@@ -0,0 +1,55 @@
+namespace Late4dTrain;
+
+using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+public class OnSuccessTests
+{
+    [Fact]
+    public void OnSuccessWithIntResult()
+    {
+        var successCalled = false;
+        var failCalled = false;
+        var capturedResult = 0;
+
+        var result = Result<string>.Ok(42);
+        var func = () => result.Error;
+
+        result
+            .OnSuccess((i) =>
+                {
+                    successCalled = true;
+                    capturedResult = i;
+                }
+            )
+            .OnFailure((e) => failCalled = true);
+
+        successCalled.Should().BeTrue();
+        failCalled.Should().BeFalse();
+        result.Value.Should().Be(42);
+        capturedResult.Should().Be(42);
+        func.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void OnSuccessWithoutResult()
+    {
+        var successCalled = false;
+        var failCalled = false;
+
+        var result = Result<string>.Ok();
+        var func = () => result.Error;
+
+        result
+            .OnSuccess(() => successCalled = true)
+            .OnFailure((e) => failCalled = true);
+
+        successCalled.Should().BeTrue();
+        failCalled.Should().BeFalse();
+
+        func.Should().Throw<InvalidOperationException>();
+    }
+}
---
+++ Late4dTrain.Result/Result.cs
@@ -0,0 +1,65 @@
+﻿namespace Late4dTrain;
+
+public class Result<TError>
+{
+    private readonly TError? _error;
+
+    protected Result(bool isSuccess, TError? error)
+    {
+        if (isSuccess && error is { })
+            throw new InvalidOperationException();
+
+        if (!isSuccess && error is null)
+            throw new InvalidOperationException();
+
+        IsSuccess = isSuccess;
+        _error = error;
+    }
+
+    public TError Error => _error ?? throw new InvalidOperationException();
+
+    public bool IsSuccess { get; }
+
+    public static Result<TError> Fail(TError error) => new(false, error);
+
+    public static Result<T, TError> Fail<T>(TError error) => new(default, false, error);
+
+    public static Result<TError> Ok() => new(true, default);
+
+    public static Result<T, TError> Ok<T>(T value) => new(value, true, default);
+
+    public Result<TError> OnSuccess(Action action)
+    {
+        if (IsSuccess)
+            action();
+
+        return this;
+    }
+
+    public Result<TError> OnFailure(Action<TError> action)
+    {
+        if (!IsSuccess)
+            action(_error!);
+
+        return this;
+    }
+}
+
+public class Result<TValue, TError> : Result<TError>
+{
+    private readonly TValue? _value;
+
+    public Result(TValue? value, bool isSuccess, TError? error)
+        : base(isSuccess, error) =>
+        _value = value;
+
+    public TValue Value => !IsSuccess ? throw new InvalidOperationException(): _value!;
+
+    public Result<TValue, TError> OnSuccess(Action<TValue> action)
+    {
+        if (IsSuccess)
+            action(_value!);
+
+        return this;
+    }
+}
